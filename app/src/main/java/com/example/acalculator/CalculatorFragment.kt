e7798580ac08903132f74f9a4c7df2209ec0eb10
DianaMJesus <43983424+DianaMJesus@users.noreply.github.com>
2020-04-19 20:28:31 +0100

    Ficha 8 parcialmente realizada (falta os exercicios finais)

--- app/src/main/java/com/example/acalculator/CalculatorFragment.kt
+++ app/src/main/java/com/example/acalculator/CalculatorFragment.kt
@@ -10,17 +10,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
 import butterknife.ButterKnife
 import butterknife.OnClick
 import butterknife.Optional
 import kotlinx.android.synthetic.main.fragment_calculator.*
+import kotlinx.android.synthetic.main.fragment_calculator.view.*
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.text.SimpleDateFormat
 import java.util.*
 
 const val EXTRA_HISTORY = "com.example.acalculator.HIST"
 
-class CalculatorFragment : Fragment() {
+class CalculatorFragment : Fragment(),OnDisplayChanged {
+
+    private lateinit var viewModel: CalculatorViewModel
 
     private val TAG = MainActivity::class.java.simpleName
     private var historico = ""
@@ -28,38 +32,35 @@ class CalculatorFragment : Fragment() {
     private var horario = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_calculator, container, false)
-        ButterKnife.bind(this,view)
+        val view = inflater.inflate(R.layout.fragment_calculator,container,false)
+        viewModel = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
+        ButterKnife.bind(this, view)
         return view
+    }
+
+    override fun onStart() {
+        viewModel.registerListener(this)
+        super.onStart()
+    }
+
+    override fun onDisplayChanged(value: String?) {
+        value.let { text_visor.text = it }
+    }
+
+    override fun onDestroy() {
+        viewModel.unregisterListener()
+        super.onDestroy()
     }
 
     @Optional
     @OnClick(R.id.button_0, R.id.button_00,R.id.button_1, R.id.button_2, R.id.button_3, R.id.button_4, R.id.button_42, R.id.button_5,R.id.button_6, R.id.button_7, R.id.button_8, R.id.button_9, R.id.button_adition, R.id.button_div, R.id.button_point, R.id.button_mult, R.id.button_menos)
-    @SuppressLint("SimpleDateFormat")
     fun onClickSymbol (view: View){
-        var symbol = view.tag.toString()
-        horario = SimpleDateFormat("HH:mm:ss").format(Date())
-        Toast.makeText(activity as Context,"onClickSymbol $horario", Toast.LENGTH_SHORT).show()
-        Log.i(TAG,"Click no botão $symbol")
-        if(text_visor.text == "0") {
-            text_visor.text = symbol
-        } else {
-            text_visor.append(symbol)
-        }
+        viewModel.onClickSymbol(view.tag.toString())
     }
 
     @OnClick(R.id.button_equals)
-    @SuppressLint("SimpleDateFormat")
     fun onClickEquals(view: View){
-        horario = SimpleDateFormat("HH:mm:ss").format(Date())
-        Toast.makeText(activity as Context,"button_equals.setOnClickListener $horario", Toast.LENGTH_SHORT).show()
-        historico = text_visor.text.toString()
-        Log.i(TAG, "Click no botão =")
-        val expression = ExpressionBuilder(text_visor.text.toString()).build()
-        text_visor.text = expression.evaluate().toString()
-        operations.add(Operation(historico,text_visor.text.toString().toDouble()))
-        Log.i(TAG,"O resultado da expressão é ${text_visor.text}")
+        viewModel.onClickEquals()
     }
 
     @OnClick(R.id.button_CE)
