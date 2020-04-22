2a1ecbb326fa1cfb91893cce8af6c4e27b99e11a
DianaMJesus <43983424+DianaMJesus@users.noreply.github.com>
2020-04-22 16:04:53 +0100

    Ficha 8 parcialmente realizada (falta os exercicios finais)

---
+++ app/src/main/java/com/example/acalculator/CalculatorViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.acalculator
+
+import androidx.lifecycle.ViewModel
+import com.google.android.material.navigation.NavigationView
+
+class CalculatorViewModel : ViewModel(){
+    private val calculatorLogic = CalculatorLogic()
+    private var listener: OnDisplayChanged? = null
+    var display: String = ""
+
+    private fun notifyOnDisplayChanged(){
+        listener?.onDisplayChanged(display)
+    }
+
+    fun registerListener (listener: OnDisplayChanged){
+        this.listener = listener
+        listener.onDisplayChanged(display)
+    }
+
+    fun unregisterListener(){
+        listener = null
+    }
+
+    fun onClickSymbol (symbol: String){
+        display = calculatorLogic.insertSymbol(display,symbol)
+        notifyOnDisplayChanged()
+    }
+
+    fun onClickEquals(){
+        val result = calculatorLogic.performOperation(display)
+        display = result.toString()
+        notifyOnDisplayChanged()
+    }
+}
